32ca8563d1b106fa17eb692cedd9b28cc495db86
Justin Jones <justinjones1500@yahoo.com>
2013-10-28 16:25:35 -0400

    Add email format validations to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,5 +26,26 @@ describe User do
 	describe "when email is not present" do
 		before { @user.email = " "}
 		it {should_not be_valid}
-	end # end email is not present	
+	end # end email is not present
+
+ 	describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user).not_to be_valid
+      end
+    end
+  end # end email format is not valid
+
+ 	describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end # end email format is valid
 end # User
