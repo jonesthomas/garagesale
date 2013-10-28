564a60dff6b403e94e0142ca8cbe3b390f4f964e
Justin Jones <justinjones1500@yahoo.com>
2013-10-28 10:35:05 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,17 +7,20 @@ describe "Static pages" do
    describe "Home page" do
 
     it "should have the content 'Eudora'" do
-      visit '/static_pages/home'
+      #visit '/static_pages/home'
+			visit root_path
       expect(page).to have_content('Eudora')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
+      #visit '/static_pages/home'
+			visit root_path
       expect(page).to have_title("Eudora")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      #visit '/static_pages/home'
+			visit root_path
       expect(page).not_to have_title('| Home')
     end
   end # end home
@@ -25,12 +28,14 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      #visit '/static_pages/help'
+			visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      #visit '/static_pages/help'
+			visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end
@@ -38,12 +43,14 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      #visit '/static_pages/about'
+			visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      #visit '/static_pages/about'
+			visit about_path
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
@@ -51,12 +58,14 @@ describe "Static pages" do
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      #visit '/static_pages/contact'
+			visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
+      #visit '/static_pages/contact'
+			visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end
   end # end Contact page
@@ -64,12 +73,14 @@ describe "Static pages" do
   describe "Browse page" do
 
     it "should have the content 'Browse'" do
-      visit '/static_pages/browse'
+      #visit '/static_pages/browse'
+			visit browse_path
       expect(page).to have_content('Browse')
     end
 
     it "should have the title 'Browse'" do
-      visit '/static_pages/browse'
+      #visit '/static_pages/browse'
+			visit browse_path
       expect(page).to have_title("#{base_title} | Browse")
     end
   end # end browse page
