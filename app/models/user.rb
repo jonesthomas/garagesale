6c8e8b131e81d0f915230ec2581e85450b111860
Justin Jones <justinjones1500@yahoo.com>
2013-10-29 16:18:41 -0400

    Add has_secure_password to user.rb to ensure password functionality works

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   #validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
 	#						uniqueness:true #does not account for uppercase differences
- validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  has_secure_password
 end
