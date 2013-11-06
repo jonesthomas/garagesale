f7fee8d5db39ab82e981b99be521bf1ed0e428d2
Justin Jones <justinjones1500@yahoo.com>
2013-11-06 15:52:56 -0500

    Add links for users and stats, edit user page updates with integration tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,6 +12,23 @@ describe "User pages" do
 
   end#end signup page
 
+	describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end #end page
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end #end with invalid info
+  end #end edit page
+
 	describe "profile page" do
 		let(:user) {FactoryGirl.create(:user)}
 		before {visit user_path(user)}
