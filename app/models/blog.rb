5414d37de82df6b7b94f958022c7f1503fb63c80
Justin Jones <justinjones1500@yahoo.com>
2013-11-14 15:40:36 -0500

    Add to blog words for how long ago posted and order by newest to oldest

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ActiveRecord::Base
+	default_scope -> { order('created_at DESC') }
   validates :title, presence: true, length: { maximum: 140 }
   validates :body, presence: true, length: { maximum: 10000 }
   validates :author, presence: true, length: { maximum: 50 }
