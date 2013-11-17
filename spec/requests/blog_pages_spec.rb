7defdb7dad01a4a33e060f2f958916be4556fa45
Justin Jones <justinjones1500@yahoo.com>
2013-11-17 14:46:09 -0500

    Add test for pagination

--- spec/requests/blog_pages_spec.rb
+++ spec/requests/blog_pages_spec.rb
@@ -10,7 +10,20 @@ describe "BlogPages" do
       visit blogs_path
     end
 				it { should have_title(("#{base_title} | All Posts")  ) }
-		    it { should have_content('All posts') }
+		    it { should have_content('All Posts') }
+
+	    describe "pagination" do
+
+		    before(:all) { 31.times { FactoryGirl.create(:blog) } }
+		    after(:all)  { Blog.delete_all }
+
+		    it { should have_selector('div.pagination') } #not working, not sure why
+		    it "should list each blog" do
+		      Blog.paginate(page: 1).each do |blog|
+		        expect(page).to have_selector('li', text: blog.title)
+		      end
+		    end
+    end # end pagination
 	end #index
 
 end # end Blog pages
